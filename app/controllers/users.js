a2f5474095451d29052526d927be07f5fb5587b7
iMears <maxwellmears@gmail.com>
2018-07-12 10:43:53 -0700

    Create delete action for users

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,11 +2,11 @@ const User = require('../models/user');
 
 async function create(req, res, next) {
   try {
-    const user = await User.create({ ...req.body });
+    const user = await User.create(req.body);
 
     res.json(user);
   } catch({ message }) {
-    res.status(500).send({ message })
+    res.status(500).send({ message });
   }
 };
 
@@ -16,16 +16,20 @@ async function destroy(req, res, next) {
   try {
     const user = await User.deleteOne({ _id: id });
 
-    res.json(user);
+    res.status(204).send({});
   } catch({ message }) {
-    res.status(500).send({ message })
+    res.status(500).send({ message });
   }
 };
 
 async function index(req, res, next) {
-  const users = await User.find();
+  try {
+    const users = await User.find();
 
-  res.json(users);
+    res.json(users);
+  } catch({ message }) {
+    res.status(500).send({ message });
+  }
 };
 
 async function show(req, res, next) {
@@ -35,15 +39,27 @@ async function show(req, res, next) {
     const user = await User.findById({ _id: id });
 
     res.json(user);
-  } catch(error) {
-    res.status(404).send({ message: `User with id ${id} was not found` })
+  } catch({ message }) {
+    res.status(500).send({ message });
   }
 };
 
+async function update(req, res, next) {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findOneAndUpdate({ _id: id }, req.body, { new: true });
+
+    res.json(user);
+  } catch(error) {
+    res.status(500).send({ message });
+  }
+};
 
 module.exports = {
   create,
   destroy,
   index,
   show,
+  update,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { users: { create, destroy, index, show } } = require('../app/controllers');
+const { users: { create, destroy, index, show, update } } = require('../app/controllers');
 
 router.post('/', create);
 router.get('/', index);
 router.get('/:id', show);
 router.delete('/:id', destroy);
+router.put('/:id', update);
 
 module.exports = router;
