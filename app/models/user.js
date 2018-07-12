22d556c57ec62b40c68dfdf0c48764fa41ae3a5e
iMears <maxwellmears@gmail.com>
2018-07-12 03:48:03 -0700

    Get rid of the _id and __v attributes on the User model

--- app/models/user.js
+++ app/models/user.js
@@ -9,4 +9,12 @@ UserSchema.methods.fullName = function() {
   return `${this.firstName} ${this.lastName}`;
 }
 
+UserSchema.set('toJSON', {
+  transform: (doc, ret, options) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
 module.exports = mongoose.model('User', UserSchema);
